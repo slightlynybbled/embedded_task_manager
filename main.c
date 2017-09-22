66044b4c5b7d226c7e953c2dce659e0322ab2cd0
Jason <slightlynybbled@users.noreply.github.com>
2017-09-21 23:55:22 -0400

    Fixed spacing

--- main.c
+++ main.c
@@ -4,19 +4,19 @@
 #include "task.h"
 
 /* defines to make things a bit more readable */
-#define LED1_ON		(P1OUT |= BIT0)
-#define LED1_OFF    (P1OUT &= ~BIT0)
-#define LED1_IS_ON	((P1OUT & BIT0) > 0)
+#define LED1_ON	   (P1OUT |= BIT0)
+#define LED1_OFF   (P1OUT &= ~BIT0)
+#define LED1_IS_ON ((P1OUT & BIT0) > 0)
 
-#define LED2_ON		(P1OUT |= BIT6)
-#define LED2_OFF	(P1OUT &= ~BIT6)
-#define LED2_IS_ON  ((P1OUT & BIT6) > 0)
+#define LED2_ON	   (P1OUT |= BIT6)
+#define LED2_OFF   (P1OUT &= ~BIT6)
+#define LED2_IS_ON ((P1OUT & BIT6) > 0)
 
-#define LED_1_BLINK_PERIOD	200
-#define LED_2_BLINK_PERIOD	400
-#define BTN_POLL_PERIOD		9
+#define LED_1_BLINK_PERIOD 200
+#define LED_2_BLINK_PERIOD 400
+#define BTN_POLL_PERIOD    9
 
-#define SWITCH_IS_ON	((P1IN & BIT3) > 0)
+#define SWITCH_IS_ON ((P1IN & BIT3) > 0)
 
 /* function declarations - each will be assigned as a 'task' */
 void blinkLed1(void);
@@ -33,68 +33,68 @@ int main(void) {
 
     /* setup the LED pins and switch */
     P1DIR &= ~BIT3;
-	P1DIR |= (BIT0 | BIT6);
-	LED1_OFF;
-	LED2_OFF;
+    P1DIR |= (BIT0 | BIT6);
+    LED1_OFF;
+    LED2_OFF;
 
-	/* add the debounce task */
-	TASK_add(&debounceButton, BTN_POLL_PERIOD);
+    /* add the debounce task */
+    TASK_add(&debounceButton, BTN_POLL_PERIOD);
 
-	/* enter the task execute - this will never return! */
+    /* enter the task execute - this will never return! */
     TASK_manage();
 
-	return 0;
+    return 0;
 }
 
 void blinkLed1(void){
-	if(LED1_IS_ON){
-		LED1_OFF;
-	}else{
-		LED1_ON;
-	}
+    if(LED1_IS_ON){
+        LED1_OFF;
+    }else{
+        LED1_ON;
+    }
 }
 
 void blinkLed2(void){
-	if(LED2_IS_ON){
-		LED2_OFF;
-	}else{
-		LED2_ON;
-	}
+    if(LED2_IS_ON){
+        LED2_OFF;
+    }else{
+        LED2_ON;
+    }
 }
 
 void debounceButton(void){
-	static bool btnState = false;
-	static int8_t btnCounter = 0;
-	const int8_t btnCounterMax = 6;
-	static bool ledState = false;
+    static bool btnState = false;
+    static int8_t btnCounter = 0;
+    const int8_t btnCounterMax = 6;
+    static bool ledState = false;
+	
+    /* when switch is on, increment btnCounter, else decrement */
+    if(SWITCH_IS_ON){
+        btnCounter++;
+    }else{
+        btnCounter--;
+    }
 
-	/* when switch is on, increment btnCounter, else decrement */
-	if(SWITCH_IS_ON){
-		btnCounter++;
-	}else{
-		btnCounter--;
-	}
-
-	if((btnCounter == btnCounterMax) && (btnState == false)){
-		/* the btnCounter just counted up to this point and this
-		 * is the positive going edge - do something here */
-		btnState = true;
-		if(ledState){
-			TASK_remove(&blinkLed2);
-			TASK_add(&blinkLed1, LED_1_BLINK_PERIOD);
-			LED2_OFF;
-			ledState = false;
-		}else{
-			TASK_remove(&blinkLed1);
-			TASK_add(&blinkLed2, LED_2_BLINK_PERIOD);
-			LED1_OFF;
-			ledState = true;
-		}
-	}else if(btnCounter > btnCounterMax){
-		btnState = true;
-		btnCounter = btnCounterMax;
-	}else if(btnCounter < -btnCounterMax){
-		btnState = false;
-		btnCounter = -btnCounterMax;
-	}
+    if((btnCounter == btnCounterMax) && (btnState == false)){
+        /* the btnCounter just counted up to this point and this
+         * is the positive going edge - do something here */
+        btnState = true;
+        if(ledState){
+            TASK_remove(&blinkLed2);
+            TASK_add(&blinkLed1, LED_1_BLINK_PERIOD);
+            LED2_OFF;
+            ledState = false;
+        }else{
+            TASK_remove(&blinkLed1);
+            TASK_add(&blinkLed2, LED_2_BLINK_PERIOD);
+            LED1_OFF;
+            ledState = true;
+        }
+    }else if(btnCounter > btnCounterMax){
+        btnState = true;
+        btnCounter = btnCounterMax;
+    }else if(btnCounter < -btnCounterMax){
+        btnState = false;
+        btnCounter = -btnCounterMax;
+    }
 }
